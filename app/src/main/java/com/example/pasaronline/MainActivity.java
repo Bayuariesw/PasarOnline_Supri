5923c2d625a133a026426b12197c5c3dc37e2698
BayuAries <62095092+BayuAries@users.noreply.github.com>
2020-12-02 18:35:45 +0700

    Initial commit First

---
+++ app/src/main/java/com/example/pasaronline/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.pasaronline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button logout = findViewById(R.id.btnLogOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+//        if(v.getId() == R.id.btnAdd){
+//            Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+//            startActivity(intent);
+//        }
+    }
+}
---
+++ app/src/main/java/com/example/pasaronline/RegisterUser.java
@@ -0,0 +1,105 @@
+package com.example.pasaronline;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterUser extends AppCompatActivity {
+
+    EditText nama, email, password, telp;
+    Button btnRegister,btnLogin,btnReisterKios;
+    boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        nama = findViewById(R.id.etNama);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+        telp = findViewById(R.id.etTelp);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnReisterKios = findViewById(R.id.btnRegisterKios);
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                checkField(nama);
+                checkField(email);
+                checkField(password);
+                checkField(telp);
+
+                if (valid){
+                    //mulai user registrasi
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            Toast.makeText(RegisterUser.this, "Akun dibuat", Toast.LENGTH_SHORT).show();
+                            DocumentReference df = fStore.collection("Users").document(user.getUid());
+                            Map<String,Object> userInfo = new HashMap<>();
+                            userInfo.put("Name", nama.getText().toString());
+                            userInfo.put("Email", email.getText().toString());
+                            userInfo.put("Telp", telp.getText().toString());
+                            //
+                            userInfo.put("isUser", "1");
+                            df.set(userInfo);
+
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            finish();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(RegisterUser.this, "Gagal Membuat Akun", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+    }
+
+    private boolean checkField(EditText textField) {
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+
+}
