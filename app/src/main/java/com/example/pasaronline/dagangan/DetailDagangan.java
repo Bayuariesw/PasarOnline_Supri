922b7f8223d5a3ff09f05b991fc12e586b6d18b2
BayuAries <62095092+BayuAries@users.noreply.github.com>
2020-12-08 15:46:31 +0700

    botton tambah banyak

--- app/src/main/java/com/example/pasaronline/dagangan/DetailDagangan.java
+++ app/src/main/java/com/example/pasaronline/dagangan/DetailDagangan.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.pasaronline.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
 import static com.example.pasaronline.fragment.HomeFragment.EXTRA_DESKRIPSI;
@@ -17,11 +20,17 @@ import static com.example.pasaronline.fragment.HomeFragment.EXTRA_JUMLAH;
 import static com.example.pasaronline.fragment.HomeFragment.EXTRA_NAMA_BARANG;
 import static com.example.pasaronline.fragment.HomeFragment.EXTRA_URL;
 
-public class DetailDagangan extends AppCompatActivity {
+public class DetailDagangan extends AppCompatActivity implements View.OnClickListener{
 
+    private int total = 0;
     private ImageView imgView;
-    private TextView nama, harga, deskripsi, jumlah;
-
+    private TextView nama, harga, deskripsi, jumlah, banyak;
+    private String jmlBarang;
+    private String descBarang;
+    private String hargaBarang;
+    private String namaBarang;
+    private String imgUrl;
+    private Button tambahKeranjang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +42,15 @@ public class DetailDagangan extends AppCompatActivity {
         harga = findViewById(R.id.hargaTv);
         deskripsi = findViewById(R.id.deskripsiTv);
         jumlah = findViewById(R.id.jumlahTv);
+        banyak = findViewById(R.id.banyakTv);
+        tambahKeranjang = findViewById(R.id.btnKeranjang);
 
         Intent intent = getIntent();
-        String imgUrl = intent.getStringExtra(EXTRA_URL);
-        String namaBarang = intent.getStringExtra(EXTRA_NAMA_BARANG);
-        String hargaBarang = intent.getStringExtra(EXTRA_HARGA);
-        String descBarang = intent.getStringExtra(EXTRA_DESKRIPSI);
-        String jmlBarang = intent.getStringExtra(EXTRA_JUMLAH);
+        imgUrl = intent.getStringExtra(EXTRA_URL);
+        namaBarang = intent.getStringExtra(EXTRA_NAMA_BARANG);
+        hargaBarang = intent.getStringExtra(EXTRA_HARGA);
+        descBarang = intent.getStringExtra(EXTRA_DESKRIPSI);
+        jmlBarang = intent.getStringExtra(EXTRA_JUMLAH);
 
         //imgView.setImageURI(Uri.parse(imgUrl));
 
@@ -50,5 +61,42 @@ public class DetailDagangan extends AppCompatActivity {
         deskripsi.setText(descBarang);
         jumlah.setText("Jumlah : "+jmlBarang);
 
+        tambahKeranjang.setOnClickListener(this);
+
+
+    }
+
+    public void tambahSatu(View view){
+        total += 1;
+        int barang = Integer.parseInt(jmlBarang);
+        if (banyak != null){
+            if (total >= barang){
+                total = barang;
+            }
+            banyak.setText(Integer.toString(total));
+        }
+    }
+
+    public void kurangSatu(View view){
+        total -= 1;
+        if (banyak != null){
+            if (total <= 0){
+                total = 0;
+            }
+            banyak.setText(Integer.toString(total));
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnKeranjang){
+            addBarang();
+        }
+    }
+
+    private void addBarang() {
+        String banyakBarang = String.valueOf(total);
+//        String idBarang =
     }
 }
