768f9c276fd4cb59b17e49678c045e509e1c9830
BayuAries <62095092+BayuAries@users.noreply.github.com>
2020-12-08 08:36:26 +0700

    nama dan foto belom keluar, recycleview jadi

--- app/src/main/java/com/example/pasaronline/adapter/DaganganAdapter.java
+++ app/src/main/java/com/example/pasaronline/adapter/DaganganAdapter.java
@@ -1,4 +1,99 @@
 package com.example.pasaronline.adapter;
 
-public class DaganganAdapter {
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.pasaronline.MainActivity;
+import com.example.pasaronline.R;
+import com.example.pasaronline.model.Dagangan;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class DaganganAdapter extends RecyclerView.Adapter<DaganganAdapter.DagangViewHolder> {
+
+    private Context mContext;
+    private List<Dagangan> mDagang;
+
+    public DaganganAdapter(Context context, List<Dagangan> dagangans){
+        mContext = context;
+        mDagang = dagangans;
+    }
+
+    @NonNull
+    @Override
+    public DagangViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.dagang_item, parent, false);
+        return new DagangViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DagangViewHolder holder, int position) {
+        Dagangan daganganSekarang = mDagang.get(position);
+        holder.tvName.setText(daganganSekarang.getNamaDagangan());
+        holder.tvHarga.setText(daganganSekarang.getHarga());
+        holder.tvKios.setText("Nama Kios");
+//        DocumentReference df = FirebaseFirestore.getInstance().collection("Kios").document(daganganSekarang.getIdKios());
+//        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//            @Override
+//            public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                if (documentSnapshot.exists()){
+//                    String nama = documentSnapshot.getString("NameKios");
+//                    DagangViewHolder holder = null;
+//                    holder.tvKios.setText(nama);
+//                }
+//            }
+//        });
+
+//        Uri uri = Uri.parse(daganganSekarang.getmImageUri());
+//        holder.imgDagang.setImageURI(uri);
+
+        Picasso.with(mContext)
+                .load(mDagang.get(position).getmImageUri())
+                .placeholder(R.mipmap.ic_launcher)
+                .fit()
+                .centerCrop()
+                .into(holder.imgDagang);
+
+//        Glide.with(mContext)
+//                .load(daganganSekarang.getmImageUri()).into(holder.imgDagang);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDagang.size();
+    }
+
+    public class DagangViewHolder extends RecyclerView.ViewHolder{
+        public TextView tvName, tvKios, tvHarga;
+        public ImageView imgDagang;
+
+        public DagangViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvBarang);
+            tvKios = itemView.findViewById(R.id.tvKios);
+            tvHarga = itemView.findViewById(R.id.tvHarga);
+            imgDagang = itemView.findViewById(R.id.imgNew);
+
+
+        }
+    }
 }
--- app/src/main/java/com/example/pasaronline/fragment/HomeFragment.java
+++ app/src/main/java/com/example/pasaronline/fragment/HomeFragment.java
@@ -2,17 +2,38 @@ package com.example.pasaronline.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.pasaronline.R;
+import com.example.pasaronline.adapter.DaganganAdapter;
+import com.example.pasaronline.model.Dagangan;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
+    private RecyclerView mRecycle;
+    private DaganganAdapter mAdapter;
+    private ProgressBar mProgress;
+
+    private DatabaseReference mDatabaseRef;
+    private List<Dagangan> mDagang;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -31,6 +52,35 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
+        mRecycle = view.findViewById(R.id.recycleView);
+        mRecycle.setHasFixedSize(true);
+        mRecycle.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mProgress = view.findViewById(R.id.progress_circle);
+
+        mDagang = new ArrayList<>();
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("dagangan");
+
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapShot : snapshot.getChildren()){
+                    Dagangan dagang = postSnapShot.getValue(Dagangan.class);
+                    mDagang.add(dagang);
+                }
+
+                mAdapter = new DaganganAdapter(getContext(), mDagang);
+                mRecycle.setAdapter(mAdapter);
+                mProgress.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                mProgress.setVisibility(View.INVISIBLE);
+            }
+        });
         return view;
     }
 }
