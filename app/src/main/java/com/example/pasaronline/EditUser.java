d1fd32554b6fa80685f235a81f7a852c8b0d7a62
safikw <safikwidiantoro@gmail.com>
2020-12-15 19:43:33 +0700

    edituser error

--- app/src/main/java/com/example/pasaronline/EditUser.java
+++ app/src/main/java/com/example/pasaronline/EditUser.java
@@ -3,12 +3,49 @@ package com.example.pasaronline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditUser extends AppCompatActivity {
+
+    private EditText namabaru,emailbaru,telpbaru;
+    private Button edit;
+    private DatabaseReference database;
+    private FirebaseAuth auth;
+    private String cekNama, cekEmail, cekTelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
+
+        namabaru= findViewById(R.id.namabaruU);
+        emailbaru= findViewById(R.id.emailbaruU);
+        telpbaru= findViewById(R.id.telpbaruU);
+        edit = findViewById(R.id.btn_update);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
+//        getData();
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cekNama = namabaru.getText().toString();
+                cekEmail = emailbaru.getText().toString();
+                cekTelp = telpbaru.getText().toString();
+
+//                if(isEmpty(cekNama)||isEmpty(cekEmail)||isEmpty(cekTelp)){
+                    Toast.makeText(EditUser.this, "Diisi dong kak", Toast.LENGTH_LONG).show();
+//                }else {
+
+//                }
+            }
+        });
     }
 }
